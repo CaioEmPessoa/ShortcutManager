f90b3fbcfe9671cc517bbc1a2d23da48a3e11166
CaioEmPessoa <caiopolonioabruno@gmail.com>
2023-12-20 14:40:15 -0300

    removed edit button

--- view/app_view.py
+++ view/app_view.py
@@ -82,7 +82,6 @@ class AppWnd(ctk.CTk):
 
         edit_button = ctk.CTkButton(master=self, text="Editar", width=70,
                                     command=lambda: self.app.enable_edit())
-        edit_button.grid(row=2, column=1, pady=10)
 
         theme_buttom = ctk.CTkButton(master=self, text="Tema", width=70,
                                      command=lambda: self.app.switch_theme())

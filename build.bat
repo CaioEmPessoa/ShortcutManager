8ed160c569579e4a9e89d0d9f38bb858409e0b44
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-11-26 11:54:45 -0300

    changed build.txt to build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 pyinstaller --noconfirm --onedir --windowed --paths="C:\Users\Caio\Envs\srtc\Lib\site-packages" --icon="img/icon.ico" "run.py"
 
-*Your enviroment name might be different
+:: *Your enviroment name might be different
